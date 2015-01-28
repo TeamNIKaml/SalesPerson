96697c0155c860c8593109b47292b5b482b134c8
Nikhil Vijayakumar <nikhil.vijaykumar@gmail.com>
2015-01-28 18:30:10 +0530

    Eod commit 28-1-2015 6:30pm

--- EnergyEyeSalesPerson/src/com/energyeye/salesperson/activity/LoginActivity.java
+++ EnergyEyeSalesPerson/src/com/energyeye/salesperson/activity/LoginActivity.java
@@ -15,137 +15,115 @@ import com.energyeye.salesperson.R;
 import com.energyeye.salesperson.properties.SalesPerson;
 import com.energyeye.salesperson.webservice.LoginService;
 
-public class LoginActivity  extends Activity {
-	
-	private EditText email,password;
-	private Button loginButton;
-	private LoginService loginService;
-	private static SalesPerson user;
-	private SharedPreferences pref;
-	//private ProgressDialog  loginProgressBar;
-//	public static int progressBarStatus = 0;
-	//private Handler handler = new Handler();
-	
+public class LoginActivity extends Activity {
 
+    private EditText email, password;
+    private Button loginButton;
+    private LoginService loginService;
+    private static SalesPerson user;
+    private SharedPreferences pref;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		 
-		  
-		  setContentView(R.layout.activity_login);
-		  
-		
+    // private ProgressDialog loginProgressBar;
+    // public static int progressBarStatus = 0;
+    // private Handler handler = new Handler();
 
-		  
-		Log.e("onCreate", "chekLogin");
-		checkLoginStatus();				
-		init();			
-		loginButton.setOnClickListener(new OnClickListener() 
-		{
-			String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-			@Override
-			public void onClick(View arg0) {
-				
-				 if (email.getText().toString().matches(emailPattern) && email.getText().toString().length() > 0)
-			        { 
-					 	if(!(password.getText().toString().length()>0))
-				 		Toast.makeText(getApplicationContext(),"Password cannot be null",Toast.LENGTH_SHORT).show();					 
-			          	 				 	
-					 	Log.e("Login Activity", "dfsdfs");
-			            user = SalesPerson.getUser();
-			            user.setEmailId(email.getText().toString());
-			            user.setPassword(password.getText().toString());			              
-			         	loginService.doLogin();					 
-			        }
-			        else
-			        {
-			             Toast.makeText(getApplicationContext(),"Invalid email address",Toast.LENGTH_SHORT).show();
-			            
-			           
-			        }
-				
-			
-			}
-		});
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+
+	setContentView(R.layout.activity_login);
+
+	Log.e("onCreate", "chekLogin");
+	checkLoginStatus();
+	init();
+	loginButton.setOnClickListener(new OnClickListener() {
+	    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+	    @Override
+	    public void onClick(View arg0) {
+
+		if (email.getText().toString().matches(emailPattern)
+			&& email.getText().toString().length() > 0) {
+		    if (!(password.getText().toString().length() > 0))
+			Toast.makeText(getApplicationContext(),
+				"Password cannot be null", Toast.LENGTH_SHORT)
+				.show();
+
+		    Log.e("Login Activity", "dfsdfs");
+		    user = SalesPerson.getUser();
+		    user.setEmailId(email.getText().toString());
+		    user.setPassword(password.getText().toString());
+		    loginService.doLogin();
+		} else {
+		    Toast.makeText(getApplicationContext(),
+			    "Invalid email address", Toast.LENGTH_SHORT).show();
+
 		}
-				
-			
-			
-	
 
-	
+	    }
+	});
+    }
 
-	private void init() {
-		// TODO Auto-generated method stub
-		email =(EditText)findViewById(R.id.email);
-		password=(EditText)findViewById(R.id.password);
-		loginButton =(Button)findViewById(R.id.loginButton);
-		loginService = new LoginService(this);
-		
+    private void init() {
+	// TODO Auto-generated method stub
+	email = (EditText) findViewById(R.id.email);
+	password = (EditText) findViewById(R.id.password);
+	loginButton = (Button) findViewById(R.id.loginButton);
+	loginService = new LoginService(this);
+
+    }
+
+    @Override
+    protected void onRestart() {
+	// TODO Auto-generated method stub
+	super.onRestart();
+	Log.e("onRestart", "chekLogin");
+	checkLoginStatus();
+    }
+
+    @Override
+    protected void onResume() {
+	// TODO Auto-generated method stub
+	super.onResume();
+	email.setText("");
+	password.setText("");
+	checkLoginStatus();
+    }
+
+    @Override
+    protected void onStart() {
+	// TODO Auto-generated method stub
+	super.onStart();
+	Log.e("onStart", "chekLogin");
+	// checkLoginStatus();
+    }
+
+    @Override
+    protected void onStop() {
+	// TODO Auto-generated method stub
+	super.onStop();
+    }
+
+    private void checkLoginStatus() {
+
+	pref = getApplicationContext().getSharedPreferences(
+		"localdiskchildlocator", 0);
+	final int statusTemp = pref.getInt("loginstatus", 0);
+
+	Log.e("checkLoginStatus",
+		"LoginStatus: " + String.valueOf(statusTemp) + "\nuserKey: "
+			+ pref.getString("userKey", "") + "\nemailID: "
+			+ pref.getString("emailID", "") + "\nPassword: "
+			+ pref.getString("password", ""));
+
+	if (statusTemp == 100) {
+
+	    final Intent intent = new Intent(getApplicationContext(),
+		    PostLoginActivity.class);
+	    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	    startActivity(intent);
+
 	}
+    }
 
-
-
-	@Override
-	protected void onRestart() {
-		// TODO Auto-generated method stub
-		super.onRestart();
-		Log.e("onRestart", "chekLogin");
-		checkLoginStatus();
-	}
-
-
-
-	@Override
-	protected void onResume() {
-		// TODO Auto-generated method stub
-		super.onResume();
-		 email.setText("");
-         password.setText("");   
-		checkLoginStatus();
-	}
-
-
-
-	@Override
-	protected void onStart() {
-		// TODO Auto-generated method stub
-		super.onStart();
-		Log.e("onStart", "chekLogin");
-		//checkLoginStatus();
-	}
-
-
-
-	@Override
-	protected void onStop() {
-		// TODO Auto-generated method stub
-		super.onStop();
-	}
-	
-	private void checkLoginStatus() {
-
-		pref = getApplicationContext().getSharedPreferences(
-				"localdiskchildlocator", 0);
-		int statusTemp = pref.getInt("loginstatus", 0);
-		
-		Log.e("checkLoginStatus","LoginStatus: "+String.valueOf(statusTemp)
-				+"\nuserKey: "+pref.getString("userKey", "")
-				+"\nemailID: "+pref.getString("emailID", "")
-				+"\nPassword: "+pref.getString("password", ""));
-		
-		if (statusTemp == 100) {
-
-			Intent intent = new Intent(getApplicationContext(),
-			PostLoginActivity.class);
-			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			startActivity(intent);
-			
-		}
-	}
-
-
-
-	
 }
--- EnergyEyeSalesPerson/src/com/energyeye/salesperson/webservice/LoginService.java
+++ EnergyEyeSalesPerson/src/com/energyeye/salesperson/webservice/LoginService.java
@@ -28,160 +28,139 @@ import android.util.Log;
 import com.energyeye.salesperson.properties.Constants;
 import com.energyeye.salesperson.properties.SalesPerson;
 
-
-
-
-
-
 public class LoginService {
 
-	private static InputStream is = null;
-	private Context context;
-	private static SalesPerson user;
-	private SharedPreferences pref;
-	private Editor editor;
+    private static InputStream is = null;
+    private Context context;
+    private static SalesPerson user;
+    private SharedPreferences pref;
+    private Editor editor;
 
-	
+    public LoginService(Context context) {
+	this.context = context;
+    }
 
-	public LoginService(Context context) {
-		this.context = context;
-	}
-	
-	
-	public Context getContext() {
-		return context;
-	}
+    public Context getContext() {
+	return context;
+    }
 
+    public void setContext(Context context) {
+	this.context = context;
+    }
 
+    public void doLogin() {
+	Log.e("login Service", "ndfsdf");
+	user = SalesPerson.getUser();
 
+	new Loginwebservice().execute("login");
 
-	public void setContext(Context context) {
-		this.context = context;
-	}
+    }
 
+    public void postLogin() {
 
-	public void doLogin() {
-		Log.e("login Service", "ndfsdf");
-		user = SalesPerson.getUser();
-		
-		new Loginwebservice().execute("login");		
-	
-		
-	}
-	
-	
+	writeSharedPreference();
+	final Intent intent = new Intent(context,
+		com.energyeye.salesperson.activity.PostLoginActivity.class);
+	// intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-	public void postLogin() {
-		
-		
-		writeSharedPreference();
-		Intent intent = new Intent(context, com.energyeye.salesperson.activity.PostLoginActivity.class);
-		//intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);	
-		
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		context.startActivity(intent);		
-		
-		
-		
-	}
-	
-	private void writeSharedPreference() {
-		// TODO Auto-generated method stub
-		
-		pref = getContext().getSharedPreferences("localdiskchildlocator", 0);
-		editor = pref.edit();
-		Log.e("writeToSharedPreferences", "localdiskchildlocator");
-		editor.putString("userKey", user.getUserKey());
-		editor.putString("emailID", user.getEmailId());
-		editor.putString("password", user.getPassword());
-		editor.putInt("loginstatus", 100);		
-		editor.commit();	
-		
-	}
+	intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	context.startActivity(intent);
 
-	private class Loginwebservice extends AsyncTask<String, Integer, String> {
+    }
 
-		
+    private void writeSharedPreference() {
+	// TODO Auto-generated method stub
 
-		@Override
-		protected void onPostExecute(String result) {
-			// TODO Auto-generated method stub
-			super.onPostExecute(result);
-			 int loginStatus = 0;
-			JSONObject json_data;
-			try {
-				json_data = new JSONObject(result);
-				for (int i = 0; i < json_data.length(); i++) {
+	pref = getContext().getSharedPreferences("localdiskchildlocator", 0);
+	editor = pref.edit();
+	Log.e("writeToSharedPreferences", "localdiskchildlocator");
+	editor.putString("userKey", user.getUserKey());
+	editor.putString("emailID", user.getEmailId());
+	editor.putString("password", user.getPassword());
+	editor.putInt("loginstatus", 100);
+	editor.commit();
 
-					if (Integer.parseInt(json_data.getString("status")) == 200) {
-						 loginStatus = 200;
-						user.setUserKey(json_data.getString("userKey"));
-						postLogin();
-						
-						
-					}
-					else
-						loginStatus = Integer.parseInt(json_data.getString("status"));
-				}
+    }
 
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			 if (loginStatus == 200) {
-				    Log.e("Login Sucessful", user.getUserKey());
-				    postLogin();
-				   }else{
-					   	   Log.e("Login error",Constants.EMAIL_ID_PASSWORD_INVALID);		 
-				   }
+    private class Loginwebservice extends AsyncTask<String, Integer, String> {
+
+	@Override
+	protected void onPostExecute(String result) {
+	    // TODO Auto-generated method stub
+	    super.onPostExecute(result);
+	    int loginStatus = 0;
+	    JSONObject json_data;
+	    try {
+		json_data = new JSONObject(result);
+		for (int i = 0; i < json_data.length(); i++) {
+
+		    if (Integer.parseInt(json_data.getString("status")) == 200) {
+			loginStatus = 200;
+			user.setUserKey(json_data.getString("userKey"));
+			postLogin();
+
+		    } else
+			loginStatus = Integer.parseInt(json_data
+				.getString("status"));
 		}
 
-		@Override
-		protected String doInBackground(String... params) {
-			
-			String json = "";
-			List<BasicNameValuePair> params1 = new ArrayList<BasicNameValuePair>();
-			params1.add(new BasicNameValuePair("emailId", user.getEmailId()));
-			params1.add(new BasicNameValuePair("password", user.getPassword()));
-			params1.add(new BasicNameValuePair("select", "Login"));
-			
-
-			try {
-				
-				HttpPost httpPost = null;
-				DefaultHttpClient httpClient = new DefaultHttpClient();				
-				httpPost = new HttpPost(Constants.LOGIN_URL);				
-				httpPost.setEntity(new UrlEncodedFormEntity(params1));
-				HttpResponse httpResponse = httpClient.execute(httpPost);
-				HttpEntity httpEntity = httpResponse.getEntity();
-				
-				is = httpEntity.getContent();
-
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			} catch (ClientProtocolException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			try {
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(is, "iso-8859-1"), 8);
-				StringBuilder sb = new StringBuilder();
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					sb.append(line);
-				}
-				is.close();
-				json = sb.toString();
-				Log.e("JSON", json);
-			} catch (Exception e) {
-				Log.e("Buffer Error", "Error converting result " + e.toString());
-			}
-			
-			return json;
-		}
+	    } catch (final JSONException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	    if (loginStatus == 200) {
+		Log.e("Login Sucessful", user.getUserKey());
+		postLogin();
+	    } else {
+		Log.e("Login error", Constants.EMAIL_ID_PASSWORD_INVALID);
+	    }
 	}
 
-} 
+	@Override
+	protected String doInBackground(String... params) {
+
+	    String json = "";
+	    final List<BasicNameValuePair> params1 = new ArrayList<BasicNameValuePair>();
+	    params1.add(new BasicNameValuePair("emailId", user.getEmailId()));
+	    params1.add(new BasicNameValuePair("password", user.getPassword()));
+	    params1.add(new BasicNameValuePair("select", "Login"));
+
+	    try {
+
+		HttpPost httpPost = null;
+		final DefaultHttpClient httpClient = new DefaultHttpClient();
+		httpPost = new HttpPost(Constants.LOGIN_URL);
+		httpPost.setEntity(new UrlEncodedFormEntity(params1));
+		final HttpResponse httpResponse = httpClient.execute(httpPost);
+		final HttpEntity httpEntity = httpResponse.getEntity();
+
+		is = httpEntity.getContent();
+
+	    } catch (final UnsupportedEncodingException e) {
+		e.printStackTrace();
+	    } catch (final ClientProtocolException e) {
+		e.printStackTrace();
+	    } catch (final IOException e) {
+		e.printStackTrace();
+	    }
+
+	    try {
+		final BufferedReader reader = new BufferedReader(
+			new InputStreamReader(is, "iso-8859-1"), 8);
+		final StringBuilder sb = new StringBuilder();
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+		    sb.append(line);
+		}
+		is.close();
+		json = sb.toString();
+		Log.e("JSON", json);
+	    } catch (final Exception e) {
+		Log.e("Buffer Error", "Error converting result " + e.toString());
+	    }
+
+	    return json;
+	}
+    }
+
+}
